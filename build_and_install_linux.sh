014db63d2308bfe50d0751d79f9468cc55d7441e
Andrey Syvrachev <andrey.syvrachev@gmail.com>
2019-10-24 12:22:16 +0300

    disable logging

--- build_and_install_linux.sh
+++ build_and_install_linux.sh
@@ -5,6 +5,6 @@ aclocal
 libtoolize --force
 automake --add-missing
 autoreconf
-./configure --enable-audio-callback
+./configure --enable-audio-callback CXXFLAGS="-DTGVOIP_LOG_VERBOSITY=0"
 make -j8
 make install
--- build_and_install_mac.sh
+++ build_and_install_mac.sh
@@ -5,6 +5,6 @@ aclocal
 glibtoolize --force
 automake --add-missing
 autoreconf
-./configure --enable-audio-callback
+./configure --enable-audio-callback CXXFLAGS="-I/usr/local/Cellar/openssl@1.1/1.1.1d/include/ -DTGVOIP_LOG_VERBOSITY=0"
 make -j8
 make install
